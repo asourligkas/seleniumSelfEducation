de8241d55b130fb471ea45393e20ca1a4126e27c
Apostolis <apostolis.sourligkas@vodafone.com>
2020-07-26 16:36:26 +0300

    javascript_alerts.py added

---
+++ herokuapp/javascript_alerts.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+
+# define chromedriver path
+chromedriver_path = os.path.abspath('../../drivers') + '\chromedriver_win32\chromedriver.exe'
+
+# create a chrome (driver)
+driver = webdriver.Chrome('C:/Selenium/chromedriver.exe')
+
+# Open portal and maximize window
+driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+driver.maximize_window()
+
+#alert accept (only ok option available)
+driver.find_element_by_xpath("//*[contains(text(), 'Click for JS Alert')]").click()
+alert = driver.switch_to.alert
+print("Alert shows message: " + alert.text)
+alert.accept()
+print(driver.find_element_by_id("result").text)
+assert "You successfuly clicked an alert" == driver.find_element_by_id("result").text
+
+#alert with ok and cancel
+driver.find_element_by_xpath("//*[contains(text(), 'Click for JS Confirm')]").click()
+alert = driver.switch_to.alert
+print("Alert shows message: " + alert.text)
+alert.accept()
+print(driver.find_element_by_id("result").text)
+assert "You clicked: Ok" == driver.find_element_by_id("result").text
+
+driver.find_element_by_xpath("//*[contains(text(), 'Click for JS Confirm')]").click()
+alert = driver.switch_to.alert
+print("Alert shows message: " + alert.text)
+alert.dismiss()
+print(driver.find_element_by_id("result").text)
+assert "You clicked: Cancel" == driver.find_element_by_id("result").text
+
+#prompt alert
+driver.find_element_by_xpath(".//button[contains(@onclick, 'jsPrompt()')]").click()
+alert = driver.switch_to.alert
+print("Alert shows message: " + alert.text)
+alert.send_keys("TEST")
+alert.accept()
+print(driver.find_element_by_id("result").text)
+assert "You entered: TEST" == driver.find_element_by_id("result").text
+
+# Close driver
+driver.close()
