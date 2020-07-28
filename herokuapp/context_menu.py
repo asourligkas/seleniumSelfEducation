1fb86ca23680ece11c862ec0e612246cd5a96b61
Apostolis <apostolis.sourligkas@vodafone.com>
2020-07-28 10:47:23 +0300

    context_menu.py added and removed unused imports

---
+++ herokuapp/context_menu.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
+import os
+
+# define chromedriver path
+chromedriver_path = os.path.abspath('../../drivers') + '\chromedriver_win32\chromedriver.exe'
+
+# create a chrome (driver)
+driver = webdriver.Chrome('C:/Selenium/chromedriver.exe')
+
+# Open portal and maximize window
+driver.get("https://the-internet.herokuapp.com/context_menu")
+driver.maximize_window()
+
+# find context menu and right-click on it
+actionChains = ActionChains(driver)
+actionChains.context_click(driver.find_element_by_id("hot-spot")).perform()
+
+# navigate to alert and click ok on it
+alert = driver.switch_to.alert
+print("Alert shows message: " + alert.text)
+alert.accept()
+
+# Close driver
+driver.close()
--- herokuapp/javascript_alerts.py
+++ herokuapp/javascript_alerts.py
@@ -1,9 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from time import sleep
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import os
 
 # define chromedriver path
